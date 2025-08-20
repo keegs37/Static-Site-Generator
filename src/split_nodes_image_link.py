f39391606818c761fae0475c04e84a518b0bdfdb
keegs37 <keegs37@github.com>
2025-08-20 16:22:53 -0700

    Removed testing code from the split_nodes_image and split_nodes_link functions

--- src/split_nodes_image_link.py
+++ src/split_nodes_image_link.py
@@ -2,14 +2,6 @@ from extract_markdown_links_and_images import extract_markdown_images, extract_m
 from textnode import TextNode, TextType
 import re
 
-# [
-#     TextNode("This is text with a link ", TextType.TEXT),
-#     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
-#     TextNode(" and ", TextType.TEXT),
-#     TextNode(
-#         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
-#     ),
-# ]
 def split_nodes_image(old_nodes):
     new_nodes = []
 
@@ -65,18 +57,3 @@ def split_nodes_link(old_nodes):
 
     return new_nodes
    
-    
-
-
-#node = TextNode(
-    "test [image](https://i.imgur.com/zjjcJKZ.png) test this cool  ![image](https://i.imgur.com/zjjcJKZ.png) and ![image](https://i.imgur.com/zjjcJKZ.png) and another",
-    TextType.TEXT,
-#)
-#new_nodes = split_nodes_image([node])
-
-node = TextNode(
-    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-    TextType.TEXT,
-)
-new_nodes = split_nodes_link([node])
-print(new_nodes)
