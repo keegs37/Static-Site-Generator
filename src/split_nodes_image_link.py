b2645d4d848de755161ac45d9e52af5a47595130
keegs37 <keegs37@github.com>
2025-08-20 16:35:33 -0700

    Added comments to the split_nodes_image and split_nodes_link functions

--- src/split_nodes_image_link.py
+++ src/split_nodes_image_link.py
@@ -15,15 +15,16 @@ def split_nodes_image(old_nodes):
             continue
 
         image = extract_markdown_images(node.text)
-        tokens = re.split(r'\![^\]]+\]\([^\)]+\)', node.text)
+        tokens = re.split(r'\![^\]]+\]\([^\)]+\)', node.text) # Regex splits on the markdown image syntax, leaving just the remaining text
        
-        for i in range(len(tokens)):
-            if not tokens[i].strip() and i < len(image):
+        
+        for i in range(len(tokens)): # The length of tokens will always be the same or more than the length of image
+            if not tokens[i].strip() and i < len(image): # If tokens[i] is whitespace and there is an image available to append
                 new_nodes.append(TextNode(image[i][0], TextType.IMAGE, image[i][1]))
                 
             elif tokens[i].strip():
                 new_nodes.append(TextNode(tokens[i], TextType.TEXT))
-                if i < len(image):
+                if i < len(image): # Make sure there is an image to append before appending 
                     new_nodes.append(TextNode(image[i][0], TextType.IMAGE, image[i][1]))
 
 
